f2999297eef77f32dd0ca07589675498bada7182
kuanyuh <kuanyuhuang@qq.com>
2022-03-15 23:34:11 +0800

    vue

---
+++ blog-app/src/components/card/CardMe.vue
@@ -0,0 +1,78 @@
+<template>
+  <el-card>
+    <h1 class="me-author-name">Kevin's Blog</h1>
+    <div class="me-author-description">
+      <span><i class="el-icon-location-outline"></i> &nbsp;长沙&中南</span>
+      <span><i class="me-icon-job"></i> &nbsp;大三在读程序员</span>
+    </div>
+    <div class="me-author-tool"><!--
+      <i @click="showTool(qq)" :title="qq.title" class="iconfont icon-qq"></i>
+      <i @click="showTool(bilibili)" :title="bilibili.title" class="iconfont icon-bilibili-fill"></i>-->
+    </div>
+  </el-card>
+
+</template>
+
+<script>
+  export default {
+    name: 'CardMe',
+    data() {
+      return {
+        qq: {title: 'QQ', message: '2312531652'},
+        bilibili: {
+          title: '哔哩哔哩',
+          message: '<a target="_blank" href="https://space.bilibili.com/445291571">https://space.bilibili.com/445291571</a>'
+        },
+        github: {
+          title: 'github',
+          message: '<a target="_blank" href="https://github.com/kuanyuh">https://github.com/kuanyuh</a>'
+        }
+      }
+    },
+    methods: {
+      showTool(tool) {
+        this.$message({
+          duration: 0,
+          showClose: true,
+          dangerouslyUseHTMLString: true,
+          message: '<strong>' + tool.message + '</strong>'
+        });
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .me-author-name {
+    text-align: center;
+    font-size: 30px;
+    border-bottom: 1px solid #5FB878;
+  }
+
+  .me-author-description {
+    padding: 8px 0;
+  }
+
+  .me-icon-job {
+    padding-left: 16px;
+  }
+
+  .me-author-tool {
+    text-align: center;
+    padding-top: 10px;
+  }
+
+  .me-author-tool i {
+    cursor: pointer;
+    padding: 4px 10px;
+    font-size: 30px;
+  }
+  
+.icon-github{
+  width: 100px; height: 30px;
+  
+  background: url('data:image/svg+xml;utf8,<svg t="1647343917048" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="1446" width="200" height="200"><path d="M511.6 76.3C264.3 76.2 64 276.4 64 523.5 64 718.9 189.3 885 363.8 946c23.5 5.9 19.9-10.8 19.9-22.2v-77.5c-135.7 15.9-141.2-73.9-150.3-88.9C215 726 171.5 718 184.5 703c30.9-15.9 62.4 4 98.9 57.9 26.4 39.1 77.9 32.5 104 26 5.7-23.5 17.9-44.5 34.7-60.8-140.6-25.2-199.2-111-199.2-213 0-49.5 16.3-95 48.3-131.7-20.4-60.5 1.9-112.3 4.9-120 58.1-5.2 118.5 41.6 123.2 45.3 33-8.9 70.7-13.6 112.9-13.6 42.4 0 80.2 4.9 113.5 13.9 11.3-8.6 67.3-48.8 121.3-43.9 2.9 7.7 24.7 58.3 5.5 118 32.4 36.8 48.9 82.7 48.9 132.3 0 102.2-59 188.1-200 212.9 23.5 23.2 38.1 55.4 38.1 91v112.5c0.8 9 0 17.9 15 17.9 177.1-59.7 304.6-227 304.6-424.1 0-247.2-200.4-447.3-447.5-447.3z" p-id="1447"></path></svg>') no-repeat center;
+  
+  background-size: 50%;
+}
+</style>
